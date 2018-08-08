65d985d49622a7855436f695e0b9d155877cabb7
tawfiknasser <tawfiknasser@gmail.com>
2018-08-08 15:24:57 +0300

    setting configuration for heroku #11

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,13 +1,28 @@
 const dbConnection = require("../database/db_connection.js");
 
-const getData = cb => {
-  dbConnection.query(`SELECT * FROM review`, (err, res) => {
+const getData = (place_id,cb) => {
+  dbConnection.query(`SELECT * FROM review where place_id=$1`,[place_id], (err, res) => {
+
     if (err) {
       cb(err);
     } else {
+      console.log(res.rows);
       cb(null, res.rows);
     }
   });
 };
 
 module.exports = getData;
+
+
+
+//
+// const getDAta = require("getData");
+//
+// getdata((eror,result)=>{
+//   if(error){
+//    res.wrteHEAD(500k)
+//  }else{
+//    resultres
+//  }
+// })
